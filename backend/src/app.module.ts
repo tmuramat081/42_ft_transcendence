66fc00ae44ca6001a2bf3ad9fbd36ed11198a6fc
geniusmaaakun <51048638+geniusmaaakun@users.noreply.github.com>
2023-12-18 13:01:31 +0900

    add: backendからDB接続 (#22)
    * add: backendからDB接続
    
    * mod: コメントの追加

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,45 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+//
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as dotenv from 'dotenv';
+import * as Joi from 'joi';
 
+ // .envを読み込む
+dotenv.config();
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      //バリデーション
+      //required()は必須項目
+      validationSchema: Joi.object({
+        POSTGRESS_HOST: Joi.string().required(),
+        POSTGRESS_PORT: Joi.number().required(),
+        POSTGRESS_USER: Joi.string().required(),
+        POSTGRESS_PASSWORD: Joi.string().required(),
+        POSTGRESS_DB: Joi.string().required(),
+      }),
+    }),
+    // forRootAsync()を使って非同期接続
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('POSTGRESS_HOST'),
+        port: config.get<number>('POSTGRESS_PORT'),
+        username: config.get<string>('POSTGRESS_USER'),
+        password: config.get<string>('POSTGRESS_PASSWORD'),
+        database: config.get<string>('POSTGRESS_DB'),
+        entities: [],
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
