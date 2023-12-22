9bc8fbc2c88aa1648342a6fa5c6d5921a738493b
kaorishima <mk.kaorishima@gmail.com>
2023-12-22 14:37:06 +0900

    Add:users module & controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 //
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 import * as dotenv from 'dotenv';
 import * as Joi from 'joi';
 
@@ -39,6 +40,7 @@ dotenv.config();
         synchronize: true,
       }),
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
