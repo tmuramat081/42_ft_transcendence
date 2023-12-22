c1ca1079d9360c1aa12d53cf8241de1371e94fdd
kaorishima <mk.kaorishima@gmail.com>
2023-12-22 15:19:44 +0900

    Fix:users

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,52 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('users')
-export class UsersController {}
+import {
+    Controller,
+    Get,
+    Post,
+    Body,
+    Param,
+    Patch,
+    Delete,
+  } from '@nestjs/common';
+  import { UsersService } from './users.service'; //Serviceの定義をインポート
+  import { User } from './users.model';
+  
+  @Controller('users') //コントローラーの定義、ルートパスを指定
+  export class UsersController {
+    constructor(private readonly usersService: UsersService) {} //Serviceの定義をコンストラクターで引数に指定
+  
+    @Get() //ルートパスに対するGETリクエストを処理
+    getAll(): User[] {
+      return this.usersService.getAll();
+    }
+  
+    @Post() //ルートパスに対するPOSTリクエストを処理
+    addUser(
+      @Body('id') id: string,
+      @Body('nickname') nickname: string,
+      @Body('password') password: string,
+    ): User {
+      const user: User = {
+        id,
+        nickname,
+        password,
+        level: 0,
+      };
+      return this.usersService.addUser(user);
+    }
+  
+    @Get(':id') // /users/{id}
+    findUser(@Param('id') id: string): User {
+      return this.usersService.findUser(id);
+    }
+  
+    @Patch(':id')
+    updateLevel(@Param('id') id: string): User {
+      return this.usersService.updateLevel(id);
+    }
+  
+    @Delete(':id')
+    deleteUser(@Param('id') id: string): void {
+      this.usersService.deleteUser(id);
+    }
+  }
+  
---
+++ backend/src/users/users.model.ts
@@ -0,0 +1,6 @@
+export interface User {
+  id: string;
+  nickname: string;
+  password: string;
+  level: number;
+}
