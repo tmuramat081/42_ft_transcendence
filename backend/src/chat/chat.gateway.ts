ccfbdd2469f1ff9c622dde2fddedce796908de21
kaorishima <mk.kaorishima@gmail.com>
2023-12-26 15:09:36 +0900

    Fix:socket connected

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -3,9 +3,10 @@ import {
   WebSocketGateway,
   WebSocketServer,
   MessageBody,
+  ConnectedSocket,
 } from '@nestjs/websockets';
 import { Logger } from '@nestjs/common';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({ cors: { origin: '*' } })
 export class ChatGateway {
@@ -15,8 +16,22 @@ export class ChatGateway {
   private logger: Logger = new Logger('Gateway Log');
 
   @SubscribeMessage('message')
-  handleMessage(@MessageBody() message: string) {
-    this.logger.log('message recieved');
-    this.server.emit('update', message);
+  handleMessage(
+    @MessageBody() message: string,
+    @ConnectedSocket() socket: Socket,
+  ) {
+    this.logger.log(`message: recieved ${message}`);
+    const rooms = [...socket.rooms].slice(0);
+    this.server.to(rooms[1]).emit('update', message);
+  }
+  @SubscribeMessage('joinRoom')
+  joinOrUpdateRoom(
+    @MessageBody() roomId: string,
+    @ConnectedSocket() socket: Socket,
+  ) {
+    this.logger.log(`joinRoom: ${socket.id} joined ${roomId}`);
+    const rooms = [...socket.rooms].slice(0);
+    if (rooms.length == 2) socket.leave(rooms[1]);
+    socket.join(roomId);
   }
 }
