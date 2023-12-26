8564d57b58ee4b2109e18126b05081c60d4ed83d
kaorishima <mk.kaorishima@gmail.com>
2023-12-26 17:37:48 +0900

    FIX: duplicate connection

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -23,10 +23,21 @@ const socket = io("http://localhost:3001");
 const ChatPage: React.FC = () => {
   const [messages, setMessages] = useState<Message[]>([]);
   const [newMessage, setNewMessage] = useState<string>("");
-  const [msg, setMsg] = useState("");
-  const [roomID, setRoomID] = useState("");
+  const [roomID, setRoomID] = useState<string | null>(null);
 
-  //   const socket = io("http://localhost:3001");
+  // コンポーネントがマウントされたときのみ接続
+  useEffect(() => {
+    const socket = io("http://localhost:3001");
+
+    socket.on("connect", () => {
+      console.log("connection ID : ", socket.id);
+    });
+
+    return () => {
+      // コンポーネントがアンマウントされるときに切断
+      socket.disconnect();
+    };
+  }, []); // 空の依存配列はマウント時のみ実行
 
   const handleSendMessage = () => {
     if (newMessage.trim() !== "") {
@@ -36,22 +47,32 @@ const ChatPage: React.FC = () => {
   };
 
   const onClickSubmit = useCallback(() => {
-    console.log(newMessage);
-    socket.emit("message", newMessage);
-  }, [newMessage]);
+    socket.emit("message", { roomID, text: newMessage });
+  }, [newMessage, roomID, socket]);
 
   useEffect(() => {
     socket.on("connect", () => {
       console.log("connection ID : ", socket.id);
     });
-  }, []);
+  }, [socket]);
 
   useEffect(() => {
-    socket.on("update", (message: string) => {
-      console.log("recieved : ", message);
-      setMsg(message);
-    });
-  }, []);
+    if (roomID) {
+      socket.emit("joinRoom", roomID);
+
+      socket.on("update", (message: string) => {
+        setMessages((prevMessages) => [
+          ...prevMessages,
+          { text: message, timestamp: new Date() },
+        ]);
+      });
+
+      // ルームに参加したことをサーバーに通知
+      socket.emit("getMessages", roomID, (roomMessages: Message[]) => {
+        setMessages(roomMessages);
+      });
+    }
+  }, [roomID, socket]);
 
   useEffect(() => {
     const handleRoomChange = (newRoomID: string) => {
@@ -75,7 +96,6 @@ const ChatPage: React.FC = () => {
       <h1>Chat Page</h1>
 
       <div>
-        {/* メッセージ一覧 */}
         <ul>
           {messages.map((message, index) => (
             <li key={index}>
@@ -87,17 +107,16 @@ const ChatPage: React.FC = () => {
 
       <select
         onChange={(event) => {
-          setRoomID(event.target.value);
-          socket.emit("joinRoom", event.target.value);
+          const newRoomID = event.target.value;
+          setRoomID(newRoomID);
         }}
-        value={roomID}
+        value={roomID || ""}
       >
         <option value="">---</option>
         <option value="room1">Room1</option>
         <option value="room2">Room2</option>
       </select>
 
-      {/* 新しいメッセージの入力フォーム */}
       <div>
         <input
           type="text"
