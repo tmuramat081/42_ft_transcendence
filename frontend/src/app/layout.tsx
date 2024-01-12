5a97be8d85a09683ff7851df648b8b7809ac09cd
Taisei Muramatsu <91453112+tmuramat081@users.noreply.github.com>
2024-01-12 18:06:05 +0900

    Feature/20 フロントlinter/formatter設定, およびhuskyの導入 (#33)
    * add: フロントにprettier導入
    
    * add: フロントhusky lint-staged導入
    
    * add: linter formatter 一括適用
    
    * fix: ビルドエラー修正
    
    * fix: prettierにjsx用のルールを追加

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,19 +1,16 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from 'next';
+import React from 'react';
+import './globals.css';
 
 export const metadata: Metadata = {
   title: 'ft_transcendence',
   description: 'school 42 subject',
-}
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
