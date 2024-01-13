65aebbb1a90fd111c7c8f59d8ac02bc07c5fa07d
kaorishima <107974563+kaorishima@users.noreply.github.com>
2024-01-13 10:27:08 +0900

    Feature/26 簡易版chatページ (#35)
    * Add:chat page
    
    * Fix:socket connected
    
    * Fix:show all msg
    
    * FIX: duplicate connection
    
    * Fix: join error
    
    * Fix: chatLog
    
    * endless chatlog
    
    * each chatlog
    
    * Fix: chat for all client
    
    * Fix: chat text only
    
    * Fix:show room chat
    
    * Remove:msg
    
    * Fix: roomchange
    
    * Add: unmount update
    
    * Add: roomID & roomLog
    
    * Add: cssfile
    
    * Add: User
    
    * Add: image
    
    * Add: icon
    
    * Fix:self & other
    
    * Fix: chat box position
    
    * Delete: comment
    
    * Feature/20 フロントlinter/formatter設定, およびhuskyの導入 (#33)
    
    * add: フロントにprettier導入
    
    * add: フロントhusky lint-staged導入
    
    * add: linter formatter 一括適用
    
    * fix: ビルドエラー修正
    
    * fix: prettierにjsx用のルールを追加
    
    * Add:chat page
    
    * Fix:socket connected
    
    * Fix:show all msg
    
    * FIX: duplicate connection
    
    * Fix: join error
    
    * Fix: chatLog
    
    * endless chatlog
    
    * each chatlog
    
    * Fix: chat for all client
    
    * Fix: chat text only
    
    * Fix:show room chat
    
    * Remove:msg
    
    * Fix: roomchange
    
    * Add: unmount update
    
    * Add: roomID & roomLog
    
    * Add: cssfile
    
    * Add: User
    
    * Add: image
    
    * Add: icon
    
    * Fix:self & other
    
    * Fix: chat box position
    
    * Delete: comment
    
    ---------
    
    Co-authored-by: Taisei Muramatsu <91453112+tmuramat081@users.noreply.github.com>

---
+++ backend/src/chat/chat.gateway.ts
@@ -0,0 +1,53 @@
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  MessageBody,
+  ConnectedSocket,
+} from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import { Server, Socket } from 'socket.io';
+
+interface User {
+  ID: string;
+  name: string;
+  icon: string;
+}
+
+@WebSocketGateway({ cors: { origin: '*' } })
+export class ChatGateway {
+  @WebSocketServer()
+  server: Server;
+
+  private logger: Logger = new Logger('Gateway Log');
+
+  @SubscribeMessage('talk')
+  handleMessage(
+    @MessageBody() data: { roomID: string; sender: User; message: string },
+    @ConnectedSocket() socket: Socket,
+  ) {
+    this.logger.log(
+      `message received: ${data.roomID} ${data.sender.ID} ${data.message}`,
+    );
+    // 送信者の部屋IDを取得
+    const rooms = [...socket.rooms].slice(0);
+    // 送信者の部屋以外に送信
+    this.server.to(rooms[1]).emit('update', {
+      roomID: data.roomID,
+      sender: data.sender,
+      message: data.message,
+    });
+  }
+
+  @SubscribeMessage('joinRoom')
+  handleJoinRoom(
+    @MessageBody() roomID: string,
+    @ConnectedSocket() socket: Socket,
+  ) {
+    this.logger.log(`joinRoom: ${socket.id} joined ${roomID}`);
+    const rooms = [...socket.rooms].slice(0);
+    // 既に部屋に入っている場合は退出
+    if (rooms.length == 2) socket.leave(rooms[1]);
+    socket.join(roomID);
+  }
+}
---
+++ frontend/src/app/chat/layout.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Chat Page",
+  description: "chat page.",
+};
+
+export default function ChatLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <section>{children}</section>;
+}
