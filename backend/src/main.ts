f238c1560b030e12a41bd12b75094e60eb26b8b7
tmuramat <mt15hydrangea@gmail.com>
2024-01-31 01:22:58 +0900

    add: helmetかぶる

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // CORSを有効化
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  
+
+  // Helmet を使用（脆弱性対策）
+  app.use(helmet());
+
   await app.listen(3000);
 }
 bootstrap();
