fb75591335a6f564f8345e29451cb9245cdd384d
geniusmaaakun <ao_masa_0610@yahoo.co.jp>
2024-02-08 09:34:56 +0900

    add: UIファイルを追加

---
+++ frontend/src/app/auth/example/page.tsx
@@ -0,0 +1,10 @@
+// /* eslint-disable */
+// "use client";
+// import { useState } from 'react';
+// import SignIn from '@/components/auth/signin_mui';
+
+// export default function Page() {
+//     return (
+//         <SignIn />
+//     )
+// }
