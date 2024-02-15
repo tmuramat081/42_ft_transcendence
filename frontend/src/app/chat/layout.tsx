95fb8dd64f3e24980c38a97cacd4147e94b3e5fb
kaorishima <mk.kaorishima@gmail.com>
2024-02-15 16:54:34 +0900

    fix:deleted contents

---
+++ frontend/src/app/chat/layout.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable */
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Chat Page',
+  description: 'chat page.',
+};
+
+export default function ChatLayout({ children }: { children: React.ReactNode }) {
+  return <section>{children}</section>;
+}
