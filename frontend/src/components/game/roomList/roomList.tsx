056947a93e67748cca1e3546c0589b869a799846
Taisei Muramatsu <91453112+tmuramat081@users.noreply.github.com>
2024-02-17 21:36:46 +0900

    Feature/77 ルーム作成モーダルの実装 (#78)

--- frontend/src/components/game/roomList/roomList.tsx
+++ frontend/src/components/game/roomList/roomList.tsx
@@ -4,10 +4,12 @@ import useApi from '@/hooks/httpClient/useApi';
 import { GameRoom, ListGameRoomResponse, Pagination } from '@/types/game/roomList';
 import { User } from '@/types/user';
 import { useEffect, useState } from 'react';
-import GameRoomCard from './gameRoomCard';
+import GameRoomCard from './card/gameRoomCard';
 import styles from './roomList.module.css';
-import PaginationArea from './pagination/pagination';
+import PaginationArea from '../../common/pagination/pagination';
 import { GAME_ROOM_PAGE_COUNT } from '@/constants/game.constant';
+import { Button } from '@mui/material';
+import CreateRoomModal from './input/createRoomModal';
 
 type Props = {
   user?: User | null;
@@ -16,8 +18,12 @@ type Props = {
 export default function RoomList(_prop: Props) {
   // 現在ページ
   const [page, setPage] = useState(1);
+  // 新規登録モーダル
+  const [showModal, setShowModal] = useState(false);
   // ゲームルーム一覧
   const [gameRooms, setGameRooms] = useState<GameRoom[]>([]);
+  // 再フェッチ用フラグ
+  const [isUpdated, setIsUpdated] = useState(false);
   // ページネーション情報
   const [pagination, setPagination] = useState<Pagination>({
     total: 0,
@@ -26,7 +32,11 @@ export default function RoomList(_prop: Props) {
   });
 
   // ゲームルーム一覧取得APIコール
-  const { data: listGameRooms, isLoading } = useApi<ListGameRoomResponse>({
+  const {
+    data: listGameRooms,
+    fetchData: fetchListGameRoom,
+    isLoading,
+  } = useApi<ListGameRoomResponse>({
     path: 'game-room',
     method: HTTP_METHOD.GET,
     query: {
@@ -37,12 +47,17 @@ export default function RoomList(_prop: Props) {
 
   // カード押下時のハンドラ
   const handleClick = () => {
-    alert('ルーム入室');
+    alert('ルーム詳細・編集');
   };
 
   // ページ選択時のハンドラ
   const handleChangePage = (_event: React.ChangeEvent<unknown>, value: number) => {
     setPage(value);
+  };
+
+  // 再フェッチ用ハンドラ
+  const handleUpdate = () => {
+    setIsUpdated(!isUpdated);
   };
 
   useEffect(() => {
@@ -52,26 +67,51 @@ export default function RoomList(_prop: Props) {
     }
   }, [listGameRooms]);
 
+  useEffect(() => {
+    void fetchListGameRoom();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page, isUpdated]);
+
   const totalPage = Math.ceil(pagination.total / GAME_ROOM_PAGE_COUNT);
 
   if (isLoading) return <p>loading...</p>;
 
   return (
     <div className={styles.container}>
-      <div className={styles.roomListGrid}>
-        {gameRooms.map((room: GameRoom) => (
-          <div key={room.gameRoomId}>
-            <GameRoomCard
-              gameRoom={room}
-              onClick={handleClick}
-            />
-          </div>
-        ))}
-      </div>
-      <PaginationArea
-        count={totalPage}
-        page={page}
-        onChange={handleChangePage}
+      <section className={styles.header}>
+        {/* ヘッダー */}
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => setShowModal(true)}
+        >
+          ルーム作成
+        </Button>
+      </section>
+      <section className={styles.roomList}>
+        {/* カードリスト */}
+        <div className={styles.roomListGrid}>
+          {gameRooms.map((room: GameRoom) => (
+            <div key={room.gameRoomId}>
+              <GameRoomCard
+                gameRoom={room}
+                onClick={handleClick}
+              />
+            </div>
+          ))}
+        </div>
+        {/* ページネーション */}
+        <PaginationArea
+          count={totalPage}
+          page={page}
+          onChange={handleChangePage}
+        />
+      </section>
+      {/* ルーム作成モーダル */}
+      <CreateRoomModal
+        showModal={showModal}
+        handleSave={handleUpdate}
+        handleClose={() => setShowModal(false)}
       />
     </div>
   );
