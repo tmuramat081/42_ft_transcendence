7fccc9de825ce6bce4716e7fe8667bcfbf6a20e4
kaorishima <mk.kaorishima@gmail.com>
2024-02-22 08:51:03 +0900

    add:entity

--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChatGateway } from './chat.gateway';
-// import { RoomRepository } from './room.repository';
-// import { ChatLogRepository } from './chatlog.repository';
 import { Room } from './entities/room.entity';
 import { ChatLog } from './entities/chatlog.entity';
+import { User } from '../users/entities/user.entity';
+import { DM_User } from './entities/dm-user.entity';
+import { DirectMessage } from './entities/direct-message.entity';
+import { OnlineUsers } from './entities/online-users.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Room, ChatLog])],
+  imports: [TypeOrmModule.forFeature([Room, ChatLog, User, DM_User, DirectMessage, OnlineUsers])],
   providers: [ChatGateway],
 })
 export class ChatModule {}
