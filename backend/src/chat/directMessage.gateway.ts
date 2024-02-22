246f2e04550a4279e5cf852801be75526ee6044d
kaorishima <mk.kaorishima@gmail.com>
2024-02-22 23:01:11 +0900

    change: commented

--- backend/src/chat/directMessage.gateway.ts
+++ backend/src/chat/directMessage.gateway.ts
@@ -1,22 +1,17 @@
-import { SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import { Logger } from '@nestjs/common';
-import { Server, Socket } from 'socket.io';
-import { DirectMessage } from './entities/directMessage.entity'; // ダイレクトメッセージのエンティティ
+// import { SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+// import { Logger } from '@nestjs/common';
+// import { Server, Socket } from 'socket.io';
+// import { DirectMessage } from './entities/directMessage.entity'; // ダイレクトメッセージのエンティティ
 
-@WebSocketGateway()
-export class DirectMessageGateway {
-  @WebSocketServer() server: Server;
+// @WebSocketGateway()
+// export class DirectMessageGateway {
+//   @SubscribeMessage('sendDM')
+//   handleDirectMessage(client: Socket, payload: DirectMessage): void {
+//     this.logger.log(
+//       `Received DM from ${payload.sender.username} to ${payload.recipient.username}: ${payload.message}`,
+//     );
 
-  private logger: Logger = new Logger('DirectMessageGateway');
-
-  // クライアントからのメッセージを受信するハンドラー
-  @SubscribeMessage('sendDM')
-  handleDirectMessage(client: Socket, payload: DirectMessage): void {
-    this.logger.log(
-      `Received DM from ${payload.sender} to ${payload.recipient}: ${payload.message}`,
-    );
-
-    // 受信したメッセージを他のクライアントに送信する
-    this.server.to(payload.recipient).emit('updateDM', payload);
-  }
-}
+//     // 受信したメッセージを他のクライアントに送信する
+//     this.server.to(payload.recipient).emit('updateDM', payload);
+//   }
+// }
