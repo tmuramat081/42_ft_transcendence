243a1d8f37850fc206ee8953a5b454b5f779682a
geniusmaaakun <ao_masa_0610@yahoo.co.jp>
2024-02-26 01:21:44 +0900

    build対応

--- frontend/src/components/users/info.tsx
+++ frontend/src/components/users/info.tsx
@@ -10,13 +10,18 @@ export default function Info() {
   }, [getCurrentUser]);
 
   if (loading || !loginUser) {
-    return <p>loading...</p>
+    return (
+      <p>loading...</p>
+    );
   };
 
   return (
     <div>
       <h1>User Info</h1>
-      <Avatar alt={loginUser.userName} src={'http://localhost:3001/api/uploads/' + loginUser.icon} />
+      <Avatar 
+        alt={loginUser.userName} 
+        src={'http://localhost:3001/api/uploads/' + loginUser.icon} 
+       />
     
       <p>Username: {loginUser.userName}</p>
       <p>Email: {loginUser.email}</p>
