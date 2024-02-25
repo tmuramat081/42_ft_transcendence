81757743dc24f9cbfa4f7f8054838ffe816347c5
geniusmaaakun <ao_masa_0610@yahoo.co.jp>
2024-02-25 20:25:11 +0900

    build対応

--- frontend/src/components/users/update.tsx
+++ frontend/src/components/users/update.tsx
@@ -179,6 +179,7 @@ export default function Form() {
 
     const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 
+        if (!e.target.files) return;
         // URLに変換してプレビューを表示
         // mfny参考
         setFile(e.target.files[0]);
