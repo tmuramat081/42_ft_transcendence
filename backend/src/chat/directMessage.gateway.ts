b57e009e1828c8d0f65de01f7651bb6c23124c89
kaorishima <mk.kaorishima@gmail.com>
2024-02-28 15:07:19 +0900

    add: directMessage.gateway

--- backend/src/chat/directMessage.gateway.ts
+++ backend/src/chat/directMessage.gateway.ts
@@ -1,17 +1,82 @@
-// import { SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-// import { Logger } from '@nestjs/common';
-// import { Server, Socket } from 'socket.io';
-// import { DirectMessage } from './entities/directMessage.entity'; // ダイレクトメッセージのエンティティ
+/* eslint-disable */
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  MessageBody,
+  ConnectedSocket,
+} from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import { Server, Socket } from 'socket.io';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ChatLog } from './entities/chatlog.entity';
+import { Room } from './entities/room.entity';
+import { User } from '../users/entities/user.entity';
+import { CurrentUser } from './entities/currentUser.entity';
+import { DirectMessage } from './entities/directMessage.entity';
+import { OnlineUsers } from './entities/onlineUsers.entity';
 
-// @WebSocketGateway()
-// export class DirectMessageGateway {
-//   @SubscribeMessage('sendDM')
-//   handleDirectMessage(client: Socket, payload: DirectMessage): void {
-//     this.logger.log(
-//       `Received DM from ${payload.sender.username} to ${payload.recipient.username}: ${payload.message}`,
-//     );
+export interface UserInfo {
+  ID: string;
+  name: string;
+  icon: string;
+}
 
-//     // 受信したメッセージを他のクライアントに送信する
-//     this.server.to(payload.recipient).emit('updateDM', payload);
-//   }
-// }
+export interface ChatMessage {
+  user: string;
+  photo: string;
+  text: string;
+  timestamp: string;
+}
+
+@WebSocketGateway({ cors: { origin: '*' } })
+export class DMGateway {
+  @WebSocketServer()
+  server: Server;
+
+  private logger: Logger = new Logger('Gateway Log');
+  // 現在のUserの情報を保持
+  private currentUser: UserInfo = {
+    ID: '',
+    name: '',
+    icon: '',
+  };
+
+  constructor(
+    @InjectRepository(ChatLog)
+    private chatLogRepository: Repository<ChatLog>,
+
+    @InjectRepository(Room)
+    private roomRepository: Repository<Room>,
+
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+
+    @InjectRepository(CurrentUser)
+    private currentUserRepository: Repository<CurrentUser>,
+
+    @InjectRepository(DirectMessage)
+    private directMessageRepository: Repository<DirectMessage>,
+
+    @InjectRepository(OnlineUsers)
+    private onlineUsersRepository: Repository<OnlineUsers>,
+  ) {}
+
+  @SubscribeMessage('getCurrentUser')
+  async handleGetCurrentUser(@ConnectedSocket() socket: Socket) {
+    try {
+      this.logger.log(`getCurrentUser`);
+      // データベースからcurrentUserを取得
+      const currentUser = await this.currentUserRepository.findOne({});
+      if (currentUser) {
+        this.currentUser.ID = currentUser.userId;
+        this.currentUser.name = currentUser.name;
+        this.currentUser.icon = currentUser.icon;
+        this.server.to(socket.id).emit('currentUser', this.currentUser);
+      }
+    } catch (error) {
+      this.logger.error(error);
+    }
+  }
+}
