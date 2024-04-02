ef7fb9ce1bb7cd49f599e98cd1417e9279d0fd9d
tmuramat <mt15hydrangea@gmail.com>
2024-04-03 00:36:22 +0900

    DB接続情報にsslを追加

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -71,6 +71,12 @@ dotenv.config();
           Match,
         ], // 直接エンティティを指定
         synchronize: true,
+        ssl: true,
+        extra: {
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        }
       }),
     }),
     UsersModule,
