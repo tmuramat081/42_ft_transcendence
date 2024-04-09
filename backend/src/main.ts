f68bfc4355c658db91e8a0931f4b4d5004b5e361
tmuramat <mt15hydrangea@gmail.com>
2024-04-10 01:21:31 +0900

    fix: デプロイ用に外部アクセスを受け入れる

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -54,7 +54,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 
 void bootstrap();
