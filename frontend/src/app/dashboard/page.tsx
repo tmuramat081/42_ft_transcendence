e25f957ac327acd9df4af9fa157b8f509d2ae444
tmuramat <mt15hydrangea@gmail.com>
2024-04-11 04:45:17 +0900

    fix: dashboard

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,28 +1,12 @@
 'use client';
 
 import FriendList from '@/components/dashboard/friendList';
-import { APP_ROUTING } from '@/constants/routing.constant';
-import { useAuth } from '@/providers/useAuth';
 import { Grid } from '@mui/material';
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
 
 /**
  * ダッシュボード画面
  */
 export default function Page() {
-  const router = useRouter();
-  const { loginUser, loading } = useAuth();
-
-  // 初期化処理
-  useEffect(() => {
-    if (loading) return;
-    if (!loginUser) {
-      router.push(APP_ROUTING.AUTH.SIGN_IN.path);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [loginUser, loading]);
-
   return (
     <>
       <Grid
