1cd89efa174fe5a54da7fc8446a3d7ab7b373aef
tmuramat <mt15hydrangea@gmail.com>
2024-04-12 03:12:19 +0900

    docker-composeからの起動時はdevelop用のビルドを行う

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -54,7 +54,10 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT || 3000, '0.0.0.0');
+  const port = process.env.PORT || 3000;
+  await app.listen(3000, '0.0.0.0');
+ 
+  console.log(`Server running on http://localhost:${port}`);
 }
 
 void bootstrap();
