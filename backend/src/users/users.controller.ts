c5945f77f8994741cc47b902b29c6be45ad60e32
tmuramat <mt15hydrangea@gmail.com>
2024-04-14 12:27:50 +0900

    Revert "sameSite属性を設定"
    This reverts commit 49cf4f3f065c1ab1f71db6e99eb2d078a731938a.

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -183,10 +183,7 @@ export class UsersController {
             //cookieにアクセストークンを保存
             // localstrageよりcookieの方が安全
             // XSS, 有効期限の観点からもcookieの方が良い
-            res.cookie('jwt', accessToken, { 
-                httpOnly: true,
-                sameSite: 'none',
-            })
+            res.cookie('jwt', accessToken, { httpOnly: true })
 
             // //redisにアクセストークンを保存
 
@@ -325,10 +322,7 @@ export class UsersController {
                 //return res.status(400).json({ message: 'Invalid credentials' });
             }
             //cookieにアクセストークンを保存
-            res.cookie('jwt', accessToken, { 
-                httpOnly: true,
-                sameSite: 'none',
-             })
+            res.cookie('jwt', accessToken, { httpOnly: true })
 
             //console.log("accessToken: " + accessToken);
 
@@ -437,10 +431,7 @@ export class UsersController {
             //cookieにアクセストークンを保存
             // localstrageよりcookieの方が安全
             // XSS, 有効期限の観点からもcookieの方が良い
-            res.cookie('jwt', accessToken, { 
-                httpOnly: true,
-                sameSite: 'none',
-            })
+            res.cookie('jwt', accessToken, { httpOnly: true })
 
             // //redisにアクセストークンを保存
 
