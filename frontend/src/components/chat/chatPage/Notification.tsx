f504d5d63d0f85eb88018fb5242e2f6d0e419156
kaorishima <mk.kaorishima@gmail.com>
2024-05-10 17:36:04 +0900

    change: nortification

--- frontend/src/components/chat/chatPage/Notification.tsx
+++ frontend/src/components/chat/chatPage/Notification.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
-const Notification = ({ message, onClose }) => {
+interface NotificationProps {
+  message: string;
+  onClose: () => void;
+}
+
+const Notification: React.FC<NotificationProps> = ({ message, onClose }) => {
   return (
     <div className="notification">
       <p>{message}</p>
-      <button onClick={onClose}>Close</button>
     </div>
   );
 };
